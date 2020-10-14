0a22b4866bc86680d5a17f5c17c072ebe16c1f5a
Jaeruz Datiles <jet.datz@gmail.com>
2020-10-14 16:08:59 +0800

    Updates

---
+++ precache-manifest.8ef5fcac029b83421293db25da17bd06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57dc22bd131101b56b5a1d1f7862727f",
+    "url": "/barcodeTemp/index.html"
+  },
+  {
+    "revision": "2fd4408fb4f9bbd9b36f",
+    "url": "/barcodeTemp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c33bdf67be460abdcdf2",
+    "url": "/barcodeTemp/static/css/main.9b2a48be.chunk.css"
+  },
+  {
+    "revision": "2fd4408fb4f9bbd9b36f",
+    "url": "/barcodeTemp/static/js/2.f9fd370f.chunk.js"
+  },
+  {
+    "revision": "a8069aeec6fcb7d0b94ed4f73d83f364",
+    "url": "/barcodeTemp/static/js/2.f9fd370f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33bdf67be460abdcdf2",
+    "url": "/barcodeTemp/static/js/main.f5c5e846.chunk.js"
+  },
+  {
+    "revision": "996e932e3fe33273275d",
+    "url": "/barcodeTemp/static/js/runtime-main.a4d4d052.js"
+  },
+  {
+    "revision": "f3300b1ad8320c61263cbd37e1072a7c",
+    "url": "/barcodeTemp/static/media/tenor.f3300b1a.gif"
+  }
+]);
